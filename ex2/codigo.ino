d6d4445f5158afa905862f25b59a1ee2a2dcf7fc
IgorNociti <80907751+IgorNociti@users.noreply.github.com>
2022-03-18 19:04:22 -0300

    Update codigo.ino

--- ex2/codigo.ino
+++ ex2/codigo.ino
@@ -1,1 +1,18 @@
 
+//
+ configuração de vazio ()
+{
+  pinMode ( 2 ,SAÍDA);
+  pinMode ( 4 ,SAÍDA);
+}
+ loop vazio ()
+{
+       digitalWrite ( 2 ,HIGH);
+       atraso ( 1000 );
+       digitalWrite ( 2 ,LOW);
+       atraso ( 1000 );
+       digitalWrite ( 4 ,HIGH);
+       atraso ( 1000 );
+       digitalWrite ( 4 ,LOW);
+       atraso ( 1000 );
+}
