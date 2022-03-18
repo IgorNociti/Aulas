19b906366fb0198d8bfeb8e09e82e780298edcba
IgorNociti <80907751+IgorNociti@users.noreply.github.com>
2022-03-18 19:18:12 -0300

    Update codigo.ino

--- ex5/codigo.ino
+++ ex5/codigo.ino
@@ -1,1 +1,21 @@
+const  int BUTTON_PIN = 6 ;
+const  int LED_PIN1 = 3 ;
 
+int estado do botão = 0 ;
+void  configuração () {
+  pinMode (LED_PIN1, SAÍDA);
+  pinMode (BUTTON_PIN, INPUT_PULLUP);
+}
+
+void  loop () {
+  buttonState = digitalRead (BUTTON_PIN);
+  if (buttonState == LOW)
+  {
+    digitalWrite (LED_PIN1, BAIXO);
+  }
+
+
+  {
+    digitalWrite (LED_PIN1, ALTO);
+  }
+}
