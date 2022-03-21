6d26f3da6ac0ea33c4442ce92c08452f326e3431
IgorNociti <80907751+IgorNociti@users.noreply.github.com>
2022-03-21 12:49:03 -0300

    Update codigo.ino

--- ex1/codigo.ino
+++ ex1/codigo.ino
@@ -2,13 +2,13 @@
 //
 void setup()
 {
-  pinMode(5, OUTPUT);
+  pinMode(2, OUTPUT);
 }
 
 void loop()
 {
-       digitalWrite (5, HIGH);
-       delay (500);
-       digitalWrite (5, LOW); 
-       delay (500);
+  digitalWrite(2, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  delay(500);
 }
